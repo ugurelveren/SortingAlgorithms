b536fb5f42e0d5ab701aaa6dc24dc7c6f823c649
u.elveren <elveren.ugur@gmail.com>
2018-04-19 16:39:15 -0700

    QuickSort
    QuickSort Example

--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -6,7 +6,24 @@ namespace SortingAlgorithms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] a = new int[] { 5, 2, 3, 5, 6, 7, -1, -3, -6, 10, 15, 23 };
+
+            WriteToConsole(a);
+
+            QuickSort.QuickSortRecursive(ref a, 0, a.Length - 1);
+
+            Console.WriteLine();
+            WriteToConsole(a);
+            
+            
+        }
+
+        private static void WriteToConsole( int[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item + "/");
+            }
         }
     }
 }
---
+++ SortingAlgorithms/QuickSort.cs
@@ -0,0 +1,41 @@
+﻿namespace SortingAlgorithms
+{
+    class QuickSort
+    {
+        //ref is not nessesery because array is referance type but it makes readable
+        public static void QuickSortRecursive(ref int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                //we determine pivot
+                int q = Partition(ref arr, left, right);
+                QuickSortRecursive(ref arr, left, q - 1);
+                QuickSortRecursive(ref arr, q + 1, right);
+            }
+        }
+        //we swap it
+        private static int Partition(ref int[] arr, int left, int right)
+        {
+            //we pick last item as pivot
+            int pivot = arr[right];
+            int temp;
+            int i = left;
+
+            for (int j = left; j < right; ++j)
+            {
+                if (arr[j] <= pivot)
+                {
+                    temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                    i++;
+                }
+            }
+
+            arr[right] = arr[i];
+            arr[i] = pivot;
+
+            return i;
+        }
+    }
+}
